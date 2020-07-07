a257fa393cf0f43e114999fdd127063323dae9a0
TadejDragojlovic <dragojlovic.tadej@gmail.com>
2020-07-07 13:45:51 +0200

    changes

--- otp.py
+++ otp.py
@@ -14,7 +14,9 @@ class OTP:
         for i, char in enumerate(self.message): # Save positions of capital letters for later
             if char.isupper():
                 self.capitalized_positions.append(i)
+
         self.message = self.message.lower()
+        self.pad = self.pad.lower()
 
         if self.message.find(" ")>0:
             for i, char in enumerate(self.message):
@@ -22,18 +24,21 @@ class OTP:
                     self.whitespace_positions.append(i)
                     # Used to save space positions, so that later when decrypting the message, spaces can be added back
                     self.message = self.message.replace(" ", "") # Delete the spaces
-                    
+
         print(self.message)
         # /////// ADD LATER IF NEW TRIMMED MESSAGE IS NOT EQUAL TO PAD IN LENGTH, THROW AN ERROR //////////
 
     def encrypt(self):
         ciphertext = ""
+
         for i, char in enumerate(self.message):
-            curr = (ord(char)-64)+(ord(self.pad[i])-64)
+            curr = (ord(char)-96)+(ord(self.pad[i])-96)
             if curr > 26:
                 curr %= 26
-            ciphertext += chr(curr+64)
-            # print(ciphertext)
+            curr -= 1
+            ciphertext += chr(curr+96)
+
+        return ciphertext
 
     def decrypt(self):
         pass
@@ -42,5 +47,5 @@ if __name__ == "__main__":
     otp = OTP('Hello World', 'ABCDEFGHIJ')
 
     ciphertext = otp.encrypt()
-    # print(ciphertext)
+    print(ciphertext)
     # print(otp.decrypt())
